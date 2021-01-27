4286274f6cb75afa93778330244a7062b0bc7f7f
Mathilde Delanoe <openclassrooms@dlne.eu>
2021-01-27 22:02:59 +0100

    Creation du frontend avec vue.js Creation de la barre de navigation

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="nav">
+    <img alt="logo groupomania" src="./assets/icon-left-font-monochrome-white.png">
+    <router-link to="/signup">S'inscrire</router-link> |
+    <router-link to="/">Se connecter</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 10px;
+  text-align:right;
+  font-size:0.8em;
+  background-color:grey;
+
+
+
+  a {
+    color: white;
+    text-decoration:none;
+
+    &.router-link-exact-active {
+      color: blue;
+    }
+  }
+}
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    HelloWorld
+  }
+}
+</script>

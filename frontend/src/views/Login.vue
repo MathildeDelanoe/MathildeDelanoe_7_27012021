8c28756608fe6ce8c64e3ac42e2d914e9c0f57d6
Mathilde Delanoe <openclassrooms@dlne.eu>
2021-01-28 17:08:28 +0100

    Creation des formulaires d'inscription et de connexion

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -14,5 +14,49 @@ export default {
   <body class="login">
     <Nav />
 
+   <table>
+      <tbody class="text-right">
+          <tr>
+            <td><label for="emailLogin">Adresse e-mail : </label></td>
+            <td><input type="text" id="emailLogin" required></td>
+          </tr>
+          <tr>
+            <td><label for="passwordLogin">Mot de passe : </label></td>
+            <td><input type="password" id="passwordLogin" required></td>
+          </tr>
+          <tr>
+            <td colspan="2"><input type="submit" id="submitButton" value="Se connecter"></td>
+          </tr>
+      </tbody>
+  </table>
+
+
   </body>
 </template>
+
+<style lang="scss">
+  table
+  {
+    padding:30px 30px 10px 30px;
+    background-color:rgb(175,175,177);
+    border-radius:10px;
+    color:rgb(83,83,83);
+    margin:100px auto 0px auto;
+
+    td
+    {
+      text-align:left;
+      padding-bottom:20px;
+    }
+
+
+    #submitButton
+    {
+      padding:5px 15px 5px 15px;
+      background-color:white;
+      margin:auto;
+      display:block;
+    }
+  }
+
+</style>
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -14,5 +14,37 @@ export default {
   <body class="signup">
     <Nav />
 
+   <table>
+      <tbody class="text-right">
+          <tr>
+            <td><label for="lastName">Nom : </label></td>
+            <td><input type="text" id="lastName" required></td>
+          </tr>
+          <tr>
+            <td><label for="firstName">Prénom : </label></td>
+            <td><input type="text" id="firstName" required></td>
+          </tr>
+          <tr>
+            <td><label for="job">Poste occupé : </label></td>
+            <td><input type="text" id="job" required></td>
+          </tr>
+          <tr>
+            <td><label for="emailSignup">Adresse e-mail : </label></td>
+            <td><input type="text" id="emailSignup" required></td>
+          </tr>
+          <tr>
+            <td><label for="passwordSignup" class="my-4 mr-3">Mot de passe : </label></td>
+            <td><input type="password" id="passwordSignup" required></td>
+          </tr>
+          <tr>
+            <td><label for="confirmpasswordSignup" class="my-4 mr-3">Confirmation du mot de passe : </label></td>
+            <td><input type="password" id="confirmpasswordSignup" required></td>
+          </tr>
+          <tr>
+            <td colspan="2"><input type="submit" id="submitButton" value="S'inscrire" ></td>
+          </tr>
+      </tbody>
+  </table>
+
   </body>
 </template>

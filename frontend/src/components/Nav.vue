eeee3cbdb68ebe85c832ac724f22690164d72381
Mathilde Delanoe <openclassrooms@dlne.eu>
2021-01-29 23:39:22 +0100

    Implementation de l'activation du bouton de soumission du formulaire de 'signup' selon conditions Formattage du fichier Nav.vue (partie css)

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -9,49 +9,45 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav {
-  padding: 10px;
-  font-size:0.8em;
-  background-color:rgb(175,175,177);
-  display:flex;
-  justify-content:space-between;
-  align-items:center;
+  #nav {
+    padding: 10px;
+    font-size: 0.8em;
+    background-color:rgb(175,175,177);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-img {
-  height:25px;
+    img {
+      height: 25px;
     }
-                            
-  a {
-    color: rgb(83,83,83);
-    text-decoration:none;
-    
-    &.router-link-exact-active {
-      text-decoration:underline;
-      color:white;
+
+    a {
+      color: rgb(83,83,83);
+      text-decoration: none;
+
+      &.router-link-exact-active {
+        text-decoration: underline;
+        color:white;
+      }
     }
   }
-}
 
-@media screen and (min-width:768px) {
-#nav
-    {
-        padding:20px;
-        font-size:1.1em;
+  @media screen and (min-width:768px) {
+    #nav {
+      padding: 20px;
+      font-size: 1.1em;
 
-        img 
-        {
-            height:50px;
-        }
+      img {
+        height: 50px;
+      }
     }
-}
-
-
+  }
 </style>
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,15 +1,3 @@
-<script>
-import Nav from '../components/Nav.vue'
-
-export default {
-  name: 'Signup',
-  components: {
-    Nav
-  }
-}
-</script>
-
-
 <template>
   <body class="signup">
     <Nav />
@@ -18,33 +6,73 @@ export default {
       <tbody class="text-right">
           <tr>
             <td><label for="lastName">Nom : </label></td>
-            <td><input type="text" id="lastName" required></td>
+            <td><input type="text" id="lastName" required v-model="lastName"></td>
           </tr>
           <tr>
             <td><label for="firstName">Prénom : </label></td>
-            <td><input type="text" id="firstName" required></td>
+            <td><input type="text" id="firstName" required v-model="firstName"></td>
           </tr>
           <tr>
             <td><label for="job">Poste occupé : </label></td>
-            <td><input type="text" id="job" required></td>
+            <td><input type="text" id="job" required v-model="job"></td>
           </tr>
           <tr>
             <td><label for="emailSignup">Adresse e-mail : </label></td>
-            <td><input type="text" id="emailSignup" required></td>
+            <td><input type="text" id="emailSignup" required v-model="email"></td>
           </tr>
           <tr>
             <td><label for="passwordSignup" class="my-4 mr-3">Mot de passe : </label></td>
-            <td><input type="password" id="passwordSignup" required></td>
+            <td><input type="password" id="passwordSignup" required v-model="password"></td>
           </tr>
           <tr>
             <td><label for="confirmpasswordSignup" class="my-4 mr-3">Confirmation du mot de passe : </label></td>
-            <td><input type="password" id="confirmpasswordSignup" required></td>
+            <td><input type="password" id="confirmpasswordSignup" required v-model="confirmationPassword"></td>
           </tr>
           <tr>
-            <td colspan="2"><input type="submit" id="submitButton" value="S'inscrire" disabled></td>
+            <td colspan="2"><input type="submit" id="submitButton" value="S'inscrire" :disabled="this.updateAndReturnDisabled()"></td>
           </tr>
       </tbody>
   </table>
 
   </body>
 </template>
+
+<script>
+  import Nav from '../components/Nav.vue'
+
+  export default {
+    name: 'Signup',
+    components: {
+      Nav
+    },
+    data: function() {
+      return {
+        disabled: false,
+        firstName: '',
+        lastName: '',
+        job: '',
+        email: '',
+        password: '',
+        confirmationPassword: ''
+      };
+    },
+    computed: {
+      is_form_correct() {
+        const myBool = ((this.firstName.length != 0) && 
+               (this.lastName.length != 0) &&
+               (this.job.length != 0) &&
+               (this.email.length != 0) &&
+               (this.password.length != 0) &&
+               (this.confirmationPassword.length != 0) &&
+               (this.password === this.confirmationPassword));
+        return myBool
+      }
+    },
+    methods: {
+      updateAndReturnDisabled() {
+        this.disabled = !this.is_form_correct;
+        return this.disabled;
+      }
+    }
+  }
+</script>
